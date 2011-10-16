d4427bf08f0a95e440e807e63160fc51e8a041e3
mjvaanan@gmail.com <mjvaanan@gmail.com@13daca99-72e6-2eba-8695-69823e779063>
2011-10-16 13:32:02 +0000

    initial commit

---
+++ trunk/TaskRecorder/MainWindow.xaml.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Microsoft.Win32;
+
+namespace TaskRecorder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private readonly int intervalMinutes = 15;
+        private DispatcherTimer dispatcherTimer = new DispatcherTimer();
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, intervalMinutes, 0);
+        }
+
+        void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            UnAssignedMinutes += intervalMinutes;
+        }
+
+        private TaskService taskService = new TaskService();
+        public TaskService TaskService
+        {
+            get { return taskService; }
+        }
+
+        private int unAssignedMinutes;
+        public int UnAssignedMinutes
+        {
+            get { return unAssignedMinutes; }
+            set
+            {
+                unAssignedMinutes = value;
+                NotifyPropertyChanged("UnAssignedMinutes");
+            }
+        }
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private void AddTask_Click(object sender, RoutedEventArgs e)
+        {
+            TaskService.Tasks.Add(new Task { Name = "Unnamed", Date = DateTime.Now });
+        }
+
+        private void StartTimer_Click(object sender, RoutedEventArgs e)
+        {
+            dispatcherTimer.Start();
+            NotifyTimerEvents();
+        }
+
+        private void PauseTimer_Click(object sender, RoutedEventArgs e)
+        {
+            dispatcherTimer.Stop();
+            NotifyTimerEvents();
+        }
+
+        private void ResetTimer_Click(object sender, RoutedEventArgs e)
+        {
+            dispatcherTimer.Stop();
+            UnAssignedMinutes = 0;
+            NotifyTimerEvents();
+        }
+
+        private void NotifyTimerEvents()
+        {
+            NotifyPropertyChanged("TimerRunning");
+            NotifyPropertyChanged("TimerNotRunning");
+        }
+
+        public bool TimerRunning
+        {
+            get
+            {
+                return dispatcherTimer.IsEnabled;
+            }
+        }
+
+        public bool TimerNotRunning
+        {
+            get
+            {
+                return !TimerRunning;
+            }
+        }
+
+        private void DateSpinner_Spin(object sender, Microsoft.Windows.Controls.SpinEventArgs e)
+        {
+            Microsoft.Windows.Controls.ButtonSpinner spinner = (Microsoft.Windows.Controls.ButtonSpinner)sender;
+            int days = e.Direction == Microsoft.Windows.Controls.SpinDirection.Increase ? 1 : -1;
+            TaskService.CurrentDate = TaskService.CurrentDate.AddDays(days);
+
+            NotifyPropertyChanged("TimerVisibility");
+        }
+
+        public Visibility TimerVisibility
+        {
+            get
+            {
+                return TaskService.CurrentDate == DateTime.Now.Date ? Visibility.Visible : Visibility.Hidden;
+            }
+        }
+
+        private void TimeSpinner_Spin(object sender, Microsoft.Windows.Controls.SpinEventArgs e)
+        {
+            Microsoft.Windows.Controls.ButtonSpinner spinner = (Microsoft.Windows.Controls.ButtonSpinner)sender;
+            Task task = (Task)spinner.DataContext;
+
+            if (e.Direction == Microsoft.Windows.Controls.SpinDirection.Increase)
+            {
+                if (task.IsToday)
+                {
+                    if (UnAssignedMinutes >= intervalMinutes)
+                    {
+                        task.Time += intervalMinutes;
+                        UnAssignedMinutes -= intervalMinutes;
+                    }
+                }
+                else
+                {
+                    task.Time += intervalMinutes;
+                }
+            }
+            else
+            {
+                if (task.Time >= intervalMinutes)
+                {
+                    task.Time -= intervalMinutes;
+                    if (task.IsToday)
+                    {
+                        UnAssignedMinutes += intervalMinutes;
+                    }
+                }
+            }
+        }
+
+        private void dgData_PreviewExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            if (e.Command == DataGrid.DeleteCommand)
+            {
+                DataGrid dg = (DataGrid)e.Source;
+                Task task = (Task)dg.CurrentItem;
+
+                if (MessageBox.Show("Delete task \"" + task.Name + "\"?", "Confirm delete", MessageBoxButton.YesNo) == MessageBoxResult.No)
+                {
+                    e.Handled = true;
+                }
+            }
+        }
+
+        private void Export_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.FileName = "export";
+            dlg.DefaultExt = ".csv";
+            dlg.Filter = "Text files (.csv)|*.csv";
+
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                string filename = dlg.FileName;
+                try
+                {
+                    TaskService.ExportCSV(filename);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ trunk/TaskRecorder/TaskService.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Data.SqlServerCe;
+using System.Windows;
+using System.IO;
+
+namespace TaskRecorder
+{
+    public class TaskService : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public TaskService()
+        {
+            DBUtils.EnsureDBExists(CheckDBContents);
+            AttachEventHandlers();
+            SetTasksByDate();
+        }
+
+        private void CheckDBContents()
+        {
+            FindByDate(DateTime.Now.Date);
+        }
+
+        private void AttachEventHandlers()
+        {
+            tasks.ListChanged += new ListChangedEventHandler(tasks_ListChanged);
+            tasks.ItemDeleting += new DeletingItemHandler<Task>(tasks_ItemDeleting);
+        }
+
+        private void DetachEventHandlers()
+        {
+            tasks.ListChanged -= new ListChangedEventHandler(tasks_ListChanged);
+            tasks.ItemDeleting -= new DeletingItemHandler<Task>(tasks_ItemDeleting);
+        }
+
+        void tasks_ItemDeleting(object sender, Task item)
+        {
+            Delete(item);
+        }
+
+        void tasks_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            if (e.ListChangedType == ListChangedType.ItemAdded || e.ListChangedType == ListChangedType.ItemChanged)
+            {
+                Task task = Tasks[e.NewIndex];
+                if (e.ListChangedType == ListChangedType.ItemAdded)
+                {
+                    task.Date = CurrentDate;
+                }
+                InsertOrUpdate(task);
+            }
+        }
+
+        private CustomBindingList<Task> tasks = new CustomBindingList<Task>();
+        public CustomBindingList<Task> Tasks
+        {
+            get { return tasks; }
+            set
+            {
+                tasks = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("Tasks"));
+                }
+            }
+        }
+
+        private DateTime currentDate = DateTime.Now.Date;
+        public DateTime CurrentDate
+        {
+            get { return currentDate; }
+            set
+            {
+                currentDate = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("CurrentDate"));
+                }
+
+                SetTasksByDate();
+            }
+        }
+
+        private void SetTasksByDate()
+        {
+            try
+            {
+                CustomBindingList<Task> newTasks = new CustomBindingList<Task>();
+                foreach (Task task in FindByDate(currentDate))
+                {
+                    newTasks.Add(task);
+                }
+
+                DetachEventHandlers();
+                Tasks = newTasks;
+                AttachEventHandlers();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private IList<Task> FindByDate(DateTime date)
+        {
+            using (SqlCeConnection con = new SqlCeConnection(DBUtils.ConnectionString))
+            {
+                con.Open();
+
+                SqlCeCommand cmd = new SqlCeCommand("SELECT Id, Name, Category, Minutes FROM Task WHERE Date=@date", con);
+                cmd.Parameters.AddWithValue("@date", date);
+
+                IList<Task> result = new List<Task>();
+
+                SqlCeDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Task task = new Task()
+                    {
+                        Id = reader.GetString(0).ToString(),
+                        Name = reader.IsDBNull(1) ? null : reader.GetString(1),
+                        Category = reader.IsDBNull(2) ? null : reader.GetString(2),
+                        Time = reader.GetInt32(3),
+                        Date = date
+                    };
+
+                    result.Add(task);
+                }
+
+                return result;
+            }
+        }
+
+        private IList<Task> FindAll()
+        {
+            using (SqlCeConnection con = new SqlCeConnection(DBUtils.ConnectionString))
+            {
+                con.Open();
+
+                SqlCeCommand cmd = new SqlCeCommand("SELECT Id, Name, Category, Minutes, Date FROM Task ORDER BY Date, Category, Name", con);
+
+                IList<Task> result = new List<Task>();
+
+                SqlCeDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Task task = new Task()
+                    {
+                        Id = reader.GetString(0).ToString(),
+                        Name = reader.IsDBNull(1) ? null : reader.GetString(1),
+                        Category = reader.IsDBNull(2) ? null : reader.GetString(2),
+                        Time = reader.GetInt32(3),
+                        Date = reader.GetDateTime(4)
+                    };
+
+                    result.Add(task);
+                }
+
+                return result;
+            }
+        }
+
+        private void InsertOrUpdate(Task task)
+        {
+            using (SqlCeConnection con = new SqlCeConnection(DBUtils.ConnectionString))
+            {
+                con.Open();
+
+                SqlCeCommand cmd = null;
+
+                if (task.Id != null)
+                {
+                    cmd = new SqlCeCommand("UPDATE Task SET Name=@name, Category=@category, Minutes=@minutes, Date=@date WHERE Id=@id", con);
+                }
+                else
+                {
+                    cmd = new SqlCeCommand("INSERT INTO Task(Id, Name, Category, Minutes, Date) VALUES (@id, @name, @category, @minutes, @date)", con);
+                    task.Id = Guid.NewGuid().ToString();
+                }
+
+                cmd.Parameters.AddWithValue("@id", task.Id);
+                cmd.Parameters.AddWithValue("@name", task.Name);
+                cmd.Parameters.AddWithValue("@category", task.Category);
+                cmd.Parameters.AddWithValue("@minutes", task.Time);
+                cmd.Parameters.AddWithValue("@date", task.Date);
+
+                foreach (SqlCeParameter param in cmd.Parameters)
+                {
+                    if (param.Value == null)
+                    {
+                        param.Value = DBNull.Value;
+                    }
+                }
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        private void Delete(Task task)
+        {
+            using (SqlCeConnection con = new SqlCeConnection(DBUtils.ConnectionString))
+            {
+                con.Open();
+
+                if (task.Id != null)
+                {
+                    SqlCeCommand cmd = new SqlCeCommand("DELETE Task WHERE Id=@id", con);
+                    cmd.Parameters.AddWithValue("@id", task.Id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void ExportCSV(string path)
+        {
+            IList<Task> all = FindAll();
+
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                writer.WriteLine("Name,Category,Date,Minutes");
+
+                foreach (Task task in all)
+                {
+                    writer.WriteLine(task.Name + "," + task.Category + "," + task.Date.ToShortDateString() + "," + task.Time);
+                }
+            }
+        }
+    }
+}
